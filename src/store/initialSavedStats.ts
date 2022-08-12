117abf6df698ae5bdabaffd55c7244ffeafd8642
marwysoc <marta.wysocka01@gmail.com>
2022-08-12 14:49:59 +0200

    Add some initial stats

--- src/store/initialSavedStats.ts
+++ src/store/initialSavedStats.ts
@@ -1,5 +1,211 @@
 import { SavedStat } from '../components/BarChart'
 
-const savedStats: SavedStat[] = JSON.parse(localStorage.getItem('savedStats') || '[]') || []
-
-export const initialSavedStats: SavedStat[] = savedStats
+export const initialSavedStats: SavedStat[] = [
+    {
+        "id": "667817e1-7f40-44d6-9492-866bec79bcde",
+        "houseType": "Row houses",
+        "chartData": {
+            "labels": [
+                "2014K3",
+                "2014K4",
+                "2015K1",
+                "2015K2",
+                "2015K3",
+                "2015K4",
+                "2016K1",
+                "2016K2",
+                "2016K3",
+                "2016K4",
+                "2017K1",
+                "2017K2",
+                "2017K3",
+                "2017K4",
+                "2018K1",
+                "2018K2",
+                "2018K3",
+                "2018K4",
+                "2019K1",
+                "2019K2",
+                "2019K3",
+                "2019K4"
+            ],
+            "datasets": [
+                {
+                    "label": "Price [NOK]",
+                    "data": [
+                        44932,
+                        44386,
+                        46483,
+                        47085,
+                        48323,
+                        48596,
+                        50931,
+                        51780,
+                        56529,
+                        56771,
+                        61273,
+                        60726,
+                        60641,
+                        56977,
+                        59025,
+                        62175,
+                        62047,
+                        57282,
+                        62868,
+                        64308,
+                        63673,
+                        61224
+                    ],
+                    "backgroundColor": [
+                        "rgba(255, 99, 132, 0.6)",
+                        "rgba(54, 162, 235, 0.6)",
+                        "rgba(255, 206, 86, 0.6)",
+                        "rgba(75, 192, 192, 0.6)",
+                        "rgba(153, 102, 1255, 0.6)",
+                        "rgba(255, 159, 64, 0.6)",
+                        "rgba(255, 99, 132, 0.6)"
+                    ]
+                }
+            ]
+        },
+        "options": {
+            "responsive": true,
+            "maintainAspectRatio": true,
+            "plugins": {
+                "legend": {
+                    "display": true,
+                    "position": "bottom"
+                },
+                "title": {
+                    "display": true,
+                    "text": "Row houses"
+                }
+            }
+        },
+        "statOwner": "emwu",
+        "comment": {
+            "comment": "Some stats 2014-2019",
+            "commentOwner": "Marta"
+        }
+    },
+    {
+        "id": "dfd5a8ae-fe91-43d1-9e9f-d8a39eca57fd",
+        "houseType": "Multi-dwelling",
+        "chartData": {
+            "labels": [
+                "2014K1",
+                "2014K2",
+                "2014K3",
+                "2014K4",
+                "2015K1",
+                "2015K2",
+                "2015K3",
+                "2015K4",
+                "2016K1",
+                "2016K2",
+                "2016K3",
+                "2016K4"
+            ],
+            "datasets": [
+                {
+                    "label": "Price [NOK]",
+                    "data": [
+                        51573,
+                        53726,
+                        55984,
+                        55062,
+                        58726,
+                        60791,
+                        62130,
+                        60605,
+                        63818,
+                        68883,
+                        74486,
+                        76193
+                    ],
+                    "backgroundColor": [
+                        "rgba(255, 99, 132, 0.6)",
+                        "rgba(54, 162, 235, 0.6)",
+                        "rgba(255, 206, 86, 0.6)",
+                        "rgba(75, 192, 192, 0.6)",
+                        "rgba(153, 102, 1255, 0.6)",
+                        "rgba(255, 159, 64, 0.6)",
+                        "rgba(255, 99, 132, 0.6)"
+                    ]
+                }
+            ]
+        },
+        "options": {
+            "responsive": true,
+            "maintainAspectRatio": true,
+            "plugins": {
+                "legend": {
+                    "display": true,
+                    "position": "bottom"
+                },
+                "title": {
+                    "display": true,
+                    "text": "Multi-dwelling"
+                }
+            }
+        },
+        "statOwner": "emwu"
+    },
+    {
+        "id": "5e53541f-8a5c-4c34-bd4a-4339009cb8b3",
+        "houseType": "Row houses",
+        "chartData": {
+            "labels": [
+                "2019K4",
+                "2020K1",
+                "2020K2",
+                "2020K3",
+                "2020K4",
+                "2021K1",
+                "2021K2",
+                "2021K3",
+                "2021K4"
+            ],
+            "datasets": [
+                {
+                    "label": "Price [NOK]",
+                    "data": [
+                        61224,
+                        62163,
+                        65946,
+                        65861,
+                        67512,
+                        70869,
+                        76048,
+                        75785,
+                        70645
+                    ],
+                    "backgroundColor": [
+                        "rgba(255, 99, 132, 0.6)",
+                        "rgba(54, 162, 235, 0.6)",
+                        "rgba(255, 206, 86, 0.6)",
+                        "rgba(75, 192, 192, 0.6)",
+                        "rgba(153, 102, 1255, 0.6)",
+                        "rgba(255, 159, 64, 0.6)",
+                        "rgba(255, 99, 132, 0.6)"
+                    ]
+                }
+            ]
+        },
+        "options": {
+            "responsive": true,
+            "maintainAspectRatio": true,
+            "plugins": {
+                "legend": {
+                    "display": true,
+                    "position": "bottom"
+                },
+                "title": {
+                    "display": true,
+                    "text": "Row houses"
+                }
+            }
+        },
+        "statOwner": "admin2@admin.pl"
+    }
+]
